4ec937f2f4b52d975f84cf1b79774aaab2261198
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2021-11-19 07:19:00 +0530

    list component bug fix.
    Stories results has no thumbnail property and this was causing an error on stories page.

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -2,9 +2,13 @@ import Card from './card';
 
 const List = ({ list, heading }) => {
     console.log(list);
-    const listWithImagesOnly = list.filter(
-        el => !el.thumbnail.path.includes('image_not_available')
-    );
+    const listWithImagesOnly = list.filter(el => {
+        if (el.thumbnail) {
+            return !el.thumbnail.path.includes('image_not_available');
+        } else {
+            return el;
+        }
+    });
     return (
         <section className="text-white m-4 text-center">
             <h1 className="mb-8">{heading}</h1>
