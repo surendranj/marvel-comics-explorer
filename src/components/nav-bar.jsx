cd0f4ee484d5872c40e23d187b62f7e5e57d7f4f
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2021-11-19 06:03:39 +0530

    Responsive Navbar

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import marvelLogo from '../../public/images/Marvel_Logo.svg';
+import { useState, useEffect, useRef } from 'react';
 
 const NavBar = () => {
     const paths = {
@@ -11,24 +12,53 @@ const NavBar = () => {
         Series: '/series',
         Stories: '/stories',
     };
+    const [navBarDropDown, setnavBarDropDown] = useState(false);
+    const [translate, setTranslate] = useState('-translate-y-full');
+    const handleClick = () => {
+        setnavBarDropDown(!navBarDropDown);
+    };
+    useEffect(() => {
+        if (navBarDropDown) {
+            setTranslate('-translate-y-0');
+        } else if (!navBarDropDown) {
+            setTranslate('-translate-y-full');
+        }
+    }, [navBarDropDown]);
+
     return (
-        <nav className="h-full">
-            <ul className="flex flex-row justify-between h-full">
-                <ul className="ml-4 w-20 flex">
-                    <li className="flex">
-                        <a href="https://www.marvel.com/" className="flex">
-                            <Image src={marvelLogo} alt="Marvel Logo" layout="intrinsic" />
-                        </a>
+        <nav className="h-full flex justify-between text-white mx-4 items-center">
+            <div className="absolute bg-primary h-full left-0 w-full z-0 md:hidden"></div>
+            <div className="w-20 z-0">
+                <a href="https://www.marvel.com/" className="flex">
+                    <Image src={marvelLogo} alt="Marvel Logo" layout="intrinsic" />
+                </a>
+            </div>
+            <ul className={`mobile-navbar ${translate}  large-screen-navbar`}>
+                {Object.entries(paths).map(path => (
+                    <li
+                        key={path[0]}
+                        onClick={handleClick}
+                        className="border-b border-primary last:border-0 md:border-0"
+                    >
+                        <Link href={path[1]}>{path[0]}</Link>
                     </li>
-                </ul>
-                <ul className="mr-4 flex flex-row flex-grow-0.9 justify-around items-center text-white">
-                    {Object.entries(paths).map(path => (
-                        <li key={path[0]}>
-                            <Link href={path[1]}>{path[0]}</Link>
-                        </li>
-                    ))}
-                </ul>
+                ))}
             </ul>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 z-0 hover:cursor-pointer md:hidden"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={handleClick}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                />
+            </svg>
         </nav>
     );
 };
