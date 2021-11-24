0cdf880adf8c528996b686f4b407f78f69947e13
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2021-11-24 15:01:28 +0530

    Footer and card heading styling.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,10 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className="mt-auto mx-4 text-tertiary text-lg">
-            <a href="https://www.marvel.com/">Data provided by Marvel. &copy; {year} Marvel</a>
+        <footer className="mt-auto mx-4 text-tertiary text-lg ">
+            <a className="bg-white px-2 border-0 rounded" href="https://www.marvel.com/">
+                Data provided by Marvel. &copy; {year} Marvel
+            </a>
         </footer>
     );
 };
