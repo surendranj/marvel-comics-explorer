9b88a79228a0332bc084ad3d899665f7a1cbe395
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2021-11-27 08:50:04 +0530

    Changed card heading to text-base.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -12,10 +12,10 @@ const Card = ({ id, title, name, thumbnail }) => {
     return (
         // <Link href={`${router.pathname}/${id}`} passHref>
         <Link href={`${router.pathname}/${id}`} passHref>
-            <a>
-                <article className="text-white hover:cursor-pointer relative z-30 m-0.5 border-0 overflow-hidden rounded-md flex flex-col max-w-300">
+            <a className="flex flex-col">
+                <article className="text-white hover:cursor-pointer relative z-30 m-0.5 border-0 overflow-hidden rounded-md flex flex-col flex-grow max-w-300">
                     <header className="z-10 text-tertiary bg-white p-0.5 flex-grow flex items-center justify-center">
-                        <h2 className="max-w-prose truncate">{title || name}</h2>
+                        <h2 className="text-base">{title || name}</h2>
                     </header>
                     <div className="flex transform hover:scale-110 transition-transform duration-300 ease-in-out">
                         {thumbnail && (
