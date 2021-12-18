9d1dee4ac5387f9e407cee075a678d64de5a0a30
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2021-12-18 06:19:37 +0530

    Fixed Navbar appearing in 404.

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -7,6 +7,7 @@ import { RouteContext } from '../../../pages/_app';
 import useToggleNavList from '../../hooks/useToggleNavList';
 import useToggleSearchList from '../../hooks/useToggleSearchList';
 import useInputChange from '../../hooks/useInputChange';
+import { useEffect, useState } from 'react';
 
 export const NavBarContext = createContext();
 
@@ -30,13 +31,24 @@ const Layout = ({ children }) => {
         (indexOfSecondSlash === -1
             ? router.pathname
             : router.pathname.slice(0, indexOfSecondSlash));
+
+    const [is404, setIs404] = useState();
+
+    useEffect(() => {
+        if ('/404' in router.components) {
+            setIs404(true);
+        } else {
+            setIs404(false);
+        }
+    }, [router.components]);
+
     return (
         <div className="font-mouseMemoirs text-2xl min-h-screen flex flex-col">
             <Head>
                 <title>Marvel Explorer</title>
                 <link rel="icon" href="/images/logos/ironman.png" />
             </Head>
-            {router.pathname !== '/' && (
+            {router.pathname !== '/' && !router.pathname.includes('/404') && !is404 && (
                 <header className="sticky w-full top-0 z-50 shadow-customBottom h-10 bg-primary ">
                     <NavBarContext.Provider
                         value={{
